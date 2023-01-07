82d965c6e25896dc4560c7ea8ac3847d8d40bd26
jarlin8 <jarlinwei@gmail.com>
2023-01-07 16:40:17 +0800

    detectLinks: true
    Signed-off-by: jarlin8 <jarlinwei@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,11 +4,14 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
-const wiki = require('wikijs').default;
-wiki({ apiUrl: 'https://zh.wikipedia.org/w/api.php' })
-	.page('Batman')
-	.then(page => page.info('alterEgo'))
-	.then(console.log); // Bruce Wayne
+const wikipediaPreview = require('wikipedia-preview');
+wikipediaPreview.init({
+	root: document.querySelector('.content'),
+	selector: '.wiki',
+	popupContainer: '.popup-container',
+  detectLinks: true,
+	lang: 'zh'
+});
 
 const wikiLinkPlugin = require('remark-wiki-link-plus');
 const walkSync = require('walk-sync');
