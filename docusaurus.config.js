fdd92e18ecc5c6eb6b3bec396a18c8af7558ffd6
jarlin8 <jarlinwei@gmail.com>
2023-01-07 18:54:58 +0800

    up
    Signed-off-by: jarlin8 <jarlinwei@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,15 +76,12 @@ const config = {
       }),
     ],
   ],
-//  stylesheets: [
-// {
-//  href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-//  type: 'text/css',
-//    integrity:
-//      'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-//    crossorigin: 'anonymous',
-//  },
-//],  
+  stylesheets: [
+ {
+  href: 'https://cdn.jsdelivr.net/gh/jarlin8/wikiDoc@main/wikiPrevBox/wikiPreviewBox.min.css',
+  type: 'text/css',
+  },
+],  
   scripts: [ {src:"https://cdn.jsdelivr.net/gh/jarlin8/wikiDoc@main/wikiPrevBox/wikiPreviewBox.min.js"},],
   themes: [
     ['mdx-v2', {customCss: [require.resolve('./src/css/custom.css')]}],],
