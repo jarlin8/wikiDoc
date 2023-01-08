38fc34c13cbb22f7e1d22602793947b0f5fc9d39
jarlin8 <jarlinwei@gmail.com>
2023-01-08 17:35:33 +0800

    delete sync和defender
    Signed-off-by: jarlin8 <jarlinwei@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -32,8 +32,7 @@ const wikilink = [
 
 const config = {
   scripts: [
-    {src: 'https://www.matching-box.com/Scripts/quickBox.js', async: true, 'data-domain': 'www.matching-box.com'},
-    {src: 'https://niu.fendou.la/wikiPrevBox/wikiPreviewBox.min.js', async: true, 'data-domain': 'niu.fendou.la'}
+    {src: 'https://niu.fendou.la/wikiPrevBox/wikiPreviewBox.min.js','data-domain': 'niu.fendou.la'}
   ], 
 // const math = (await import('remark-math')).default;
 // const katex = (await import('rehype-katex')).default;
