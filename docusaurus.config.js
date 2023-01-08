daba3807ce7dec99fcf1b8f3ec71a18e90668eb4
jarlin8 <jarlinwei@gmail.com>
2023-01-08 17:46:44 +0800

    defer: true
    Signed-off-by: jarlin8 <jarlinwei@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -32,7 +32,7 @@ const wikilink = [
 
 const config = {
   scripts: [
-    {src: 'https://niu.fendou.la/wikiPrevBox/wikiPreviewBox.min.js','data-domain': 'niu.fendou.la'}
+    {src: 'https://niu.fendou.la/wikiPrevBox/wikiPreviewBox.min.js', defer: true, 'data-domain': 'niu.fendou.la'}
   ], 
 // const math = (await import('remark-math')).default;
 // const katex = (await import('rehype-katex')).default;
