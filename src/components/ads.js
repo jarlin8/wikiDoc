20b7ec727636485a30088c89f37dc2a2607993b2
joe <jarlinwei@gmail.com>
2024-04-24 12:28:19 +0800

    ads error

--- src/components/ads.js
+++ src/components/ads.js
@@ -1,4 +1,3 @@
-// ads.js
 import React from 'react';
 
 const adstyle = {
@@ -6,14 +5,17 @@ const adstyle = {
     width: '880px',
     height: '138px',
 };
+
 const Ads = () => {
   return (
     <a 
       href="https://s.ifttt.fun/jsmt4" 
       target="_blank" 
     >
-      <li><strong><span style="font-size: 16px;">🎈<span style="background-color: #fefefe;"><a href="https://www.ifttt.fun/go/atfx-cn" target="_blank" rel="noopener">ATFX开户</a></span>🎈</span>做外汇黄金原油，FCA监管，高流动性无滑点！</strong></li>
- 	<li><strong><span style="background-color: #fefefe;"><a style="background-color: #fefefe;" href="https://we.laowei8.com/compare-table" target="_blank" rel="noopener noreferrer" data-darkreader-inline-color="">正规交易平台排行榜</a></span><span style="font-size: 16px;">找最适合自己的平台，<span style="background-color: #fefefe;"><a style="background-color: #fefefe;" href="https://cdn.fendou.la/welaowei8/2021/08/wechat.png" target="_blank" rel="noopener">联系我！</a></span></span></strong></li>
+      <ul>
+        <li><strong><span style={{ fontSize: '16px' }}>🎈<span style={{ backgroundColor: '#fefefe' }}><a href="https://www.ifttt.fun/go/atfx-cn" target="_blank" rel="noopener">ATFX开户</a></span>🎈</span>做外汇黄金原油，FCA监管，高流动性无滑点！</strong></li>
+        <li><strong><span style={{ backgroundColor: '#fefefe' }}><a style={{ backgroundColor: '#fefefe' }} href="https://we.laowei8.com/compare-table" target="_blank" rel="noopener noreferrer" data-darkreader-inline-color="">正规交易平台排行榜</a></span><span style={{ fontSize: '16px' }}>找最适合自己的平台，<span style={{ backgroundColor: '#fefefe' }}><a style={{ backgroundColor: '#fefefe' }} href="https://cdn.fendou.la/welaowei8/2021/08/wechat.png" target="_blank" rel="noopener">联系我！</a></span></span></strong></li>
+      </ul>
       <img 
         src="https://testingcf.jsdelivr.net/gh/jarlin8/img@main/imgHD/1617860184554-forexgif.gif" 
         alt="嘉盛集团广告" 
