2032680a135b8044ed48647a276f29e484a542ac
joe <jarlinwei@gmail.com>
2024-04-24 12:48:49 +0800

    https://s.ifttt.fun/atfx up

--- src/components/ads.js
+++ src/components/ads.js
@@ -10,7 +10,7 @@ const Ads = () => {
   return (
     <div>
       <ul>
-        <li><strong><span style={{ fontSize: '16px' }}>🎈<span style={{ backgroundColor: '#fefefe' }}><a href="https://www.ifttt.fun/go/atfx-cn" target="_blank" rel="noopener">ATFX开户</a></span>🎈</span>做外汇黄金原油，FCA监管，高流动性无滑点！</strong></li>
+        <li><strong><span style={{ fontSize: '16px' }}>🎈<span style={{ backgroundColor: '#fefefe' }}><a href="https://s.ifttt.fun/atfx" target="_blank" rel="noopener">ATFX开户</a></span>🎈</span>做外汇黄金原油，FCA监管，高流动性无滑点！</strong></li>
         <li><strong><span style={{ backgroundColor: '#fefefe' }}><a style={{ backgroundColor: '#fefefe' }} href="https://we.laowei8.com/compare-table" target="_blank" rel="noopener noreferrer" data-darkreader-inline-color="">正规交易平台排行榜</a></span><span style={{ fontSize: '16px' }}>找最适合自己的平台，<span style={{ backgroundColor: '#fefefe' }}><a style={{ backgroundColor: '#fefefe' }} href="https://cdn.fendou.la/welaowei8/2021/08/wechat.png" target="_blank" rel="noopener">联系我！</a></span></span></strong></li>
       </ul>
       <a 
