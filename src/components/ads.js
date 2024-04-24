a118720183d23f8f6ed056e7bfcf6a8ed7f9bb87
joe <jarlinwei@gmail.com>
2024-04-24 11:35:52 +0800

    广告样式更新

--- src/components/ads.js
+++ src/components/ads.js
@@ -3,23 +3,19 @@ import React from 'react';
 
 const adstyle = {
     display: 'inline-block',
-    width: '728px',
-    height: '90px',
+    width: '880px',
+    height: '138px',
 };
 const Ads = () => {
   return (
     <a 
       href="https://s.ifttt.fun/jsmt4" 
       target="_blank" 
-      rel="noopener noreferrer"
       style={adstyle} // 将样式对象应用到链接上
     >
       <img 
-        className="aligncenter wp-image-24477" 
         src="https://testingcf.jsdelivr.net/gh/jarlin8/img@main/imgHD/1617860184554-forexgif.gif" 
         alt="XM外汇基础点差广告" 
-        width="870" 
-        height="224" 
         loading="lazy" 
         fetchpriority="low" 
       />
--- src/theme/TOCItems/index.js
+++ src/theme/TOCItems/index.js
@@ -6,6 +6,7 @@ export default function TOCItemsWrapper(props) {
     <>
       <TOCItems {...props} />
       <a target="_blank" href="https://www.ifttt.fun/go/atfx-cn/"><img class="size-full wp-image-24489 aligncenter" src="https://cdn.fendou.la/bluehost/ATFX-ads.svg" alt="ATFX侧边栏广告" /></a>
+      <a href="https://s.ifttt.fun/jsmt4" target="_blank" rel="noopener noreferrer" >嘉盛集团<svg width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24" class="iconExternalLink_nPIU"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg></a>
       <a target="_blank" href="https://www.ifttt.fun/go/easymarket/"><img class="size-full wp-image-24489 aligncenter" src="https://cdn.fendou.la/welaowei8/2020/11/easyMarketsAds.svg" alt="easymarkets易信侧边栏广告" /></a>
     </>
   );
