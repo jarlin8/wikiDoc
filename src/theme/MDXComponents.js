028dbea65f97ea5d7de58e26b6f3874eb92d4072
joe <jarlinwei@gmail.com>
2024-04-24 10:57:37 +0800

    更新侧边栏和插入广告

---
+++ src/theme/MDXComponents.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import MDXComponents from '@theme-original/MDXComponents';
+import Ads from '@site/src/components/ads';
+
+export default {
+  ...MDXComponents,
+  Ads
+};
---
+++ src/theme/TOCItems/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TOCItems from '@theme-original/TOCItems';
+
+export default function TOCItemsWrapper(props) {
+  return (
+    <>
+      <TOCItems {...props} />
+      <a target="_blank" href="https://www.ifttt.fun/go/atfx-cn/"><img class="size-full wp-image-24489 aligncenter" src="https://cdn.fendou.la/bluehost/ATFX-ads.svg" alt="ATFX侧边栏广告" /></a>
+      <a target="_blank" href="https://www.ifttt.fun/go/easymarket/"><img class="size-full wp-image-24489 aligncenter" src="https://cdn.fendou.la/welaowei8/2020/11/easyMarketsAds.svg" alt="easymarkets易信侧边栏广告" /></a>
+    </>
+  );
+}
