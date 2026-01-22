b519fe9165b21a8c4e76f9200b6472b8b7b630fc
Arielante Lee <jarlin8@foxmail.com>
2026-01-22 23:51:33 +0800

    ci: 移除pywencai版本锁定并统一代码格式
    - 更新 GitHub Actions 工作流，将 pywencai 依赖从固定版本 0.12.2 改为安装最新版
    - 统一 Python 脚本中的函数调用参数格式，移除多余空格以保持代码风格一致

--- wencai.py
+++ wencai.py
@@ -5,24 +5,24 @@ import os
 import re
 
 # 设置 hexin-v.js 文件的路径
-hexin_v_file = os.path.join(os.path.dirname(__file__), 'hexin-v.js')
+hexin_v_file = os.path.join(os.path.dirname(__file__),'hexin-v.js')
 
 # 获取当前日期
 today = datetime.datetime.now()
 mtime = today.strftime("%Y%m%d")
 
 # 使用 pywencai 获取股市数据
-data = pywencai.get(query='当日龙虎榜净额大于0，非st，非创业板，非科创板，非北交所，非次新股,涨幅大于-8%,涨停原因,(当日龙虎榜净额/当日龙虎榜买入金额)从大到小列出', query_type='stock', loop=True)
+data = pywencai.get(query='当日龙虎榜净额大于0，非st，非创业板，非科创板，非北交所，非次新股,涨幅大于-8%,涨停原因,(当日龙虎榜净额/当日龙虎榜买入金额)从大到小列出',query_type='stock',loop=True)
 
 # 将数据转换为 DataFrame
 df = pd.DataFrame(data)
 
 # 删除不需要的列
-columns_to_drop = ['股票市场类型', '经营范围', '上市板块','注册地址','market_code']
-df.drop(columns=columns_to_drop, inplace=True, errors='ignore')
+columns_to_drop = ['股票市场类型','经营范围','上市板块','注册地址','market_code']
+df.drop(columns=columns_to_drop,inplace=True,errors='ignore')
 
 # 修饰表头，去除 [数字] 模式以及 {} 和 ()
-df.columns = [re.sub(r'\[\d+\]|\{|\}|\(|\)', '', col) for col in df.columns]
+df.columns = [re.sub(r'\[\d+\]|\{|\}|\(|\)','',col) for col in df.columns]
 
 # 文件路径
 file_path = './docs/data_' + mtime + '.csv'
@@ -34,7 +34,7 @@ except FileNotFoundError:
     original_data = pd.DataFrame()
 
 # 合并新旧数据
-data = pd.concat([original_data, df], ignore_index=True)
+data = pd.concat([original_data,df],ignore_index=True)
 
 # 将合并后的数据保存为 CSV 文件
-data.to_csv(file_path, mode='w', index=False, header=True, encoding='utf-8-sig')
+data.to_csv(file_path,mode='w',index=False,header=True,encoding='utf-8-sig')
